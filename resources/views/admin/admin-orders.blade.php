b6aabe00f8d09c3e5024464ff2704ef0be888342
amirali <amiralighafouri80@gmail.com>
2023-05-14 15:24:19 +0430

    remove sideBar view correct adminOrder

--- resources/views/admin/admin-orders.blade.php
+++ resources/views/admin/admin-orders.blade.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\adminController;
+use App\Http\Controllers\admin\AdminUserController;
 
 ?>
 @extends('admin/master')
@@ -8,13 +8,11 @@ use App\Http\Controllers\adminController;
     <div class="bg-admin py-5">
         <div class="container">
             <h2 class="text-center bg-white py-3 rounded shadow">سفارش ها</h2>
-           <
-
            @foreach($orders->all() as $item)
                 <a href="{{route('order.list',['orderCode'=>$item->orderCode])}}">
                     <ul class="d-flex justify-content-between p-2 my-4 border border border-dark rounded bg-light text-dark shadow">
                         <div class="d-flex">
-                            @foreach(adminController::admin_order_group_list($item->orderCode)->all() as $product)
+                            @foreach(AdminUserController::admin_order_group_list($item->orderCode)->all() as $product)
                                 <li class="m-1"><img src='{{URL::asset("images/products/$product->category/$product->image")}}' class="img-fluid img-responsive rounded order-img"></li>
                             @endforeach
                         </div>
